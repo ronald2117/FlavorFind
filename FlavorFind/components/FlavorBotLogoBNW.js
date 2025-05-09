8db22ab7552c3badc244a0f1476c56366626fddb
ronald2117 <abel.ronald2117@gmail.com>
2025-05-09 23:18:55 +0800

    Refactor FlavorBotLogoBNW: update SVG structure and styling for improved rendering

--- FlavorFind/components/FlavorBotLogoBNW.js
+++ FlavorFind/components/FlavorBotLogoBNW.js
@@ -1,53 +1,54 @@
-import * as React from "react"
-import Svg, { Rect, Path } from "react-native-svg"
-
-function FlavorBotLogoBNW(props) {
-  return (
-    <Svg
-      width={25}
-      height={29}
-      viewBox="0 0 25 29"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <Rect
-        x={0.5}
-        y={4.5}
-        width={22}
-        height={14}
-        rx={1.5}
-        fill="#fff"
-        stroke="#fff"
-      />
-      <Path fill="#2A2A2A" stroke="#fff" d="M9 1H14V4H9z" />
-      <Path
-        d="M1 6a1 1 0 011-1h3v13H2a1 1 0 01-1-1V6zM5 16h17v1a1 1 0 01-1 1H5v-2z"
-        fill="#888"
-      />
-      <Path
-        d="M6 19h4a1 1 0 011 1v8a1 1 0 01-1 1H7a1 1 0 01-1-1v-9z"
-        fill="#fff"
-      />
-      <Rect x={2} y={21} width={3} height={6} rx={1} fill="#fff" />
-      <Rect
-        x={19}
-        y={24}
-        width={3}
-        height={6}
-        rx={1}
-        transform="rotate(-90 19 24)"
-        fill="#fff"
-      />
-      <Path
-        d="M13 20a1 1 0 011-1h4v9a1 1 0 01-1 1h-3a1 1 0 01-1-1v-8z"
-        fill="#fff"
-      />
-      <Path fill="#fff" d="M10 19H15V25H10z" />
-      <Rect x={9} y={7} width={4} height={6} rx={1} fill="#000" />
-      <Rect x={16} y={7} width={4} height={6} rx={1} fill="#000" />
-    </Svg>
-  )
-}
-
-export default FlavorBotLogoBNW
+import * as React from "react";
+import Svg, { Rect, Mask, Path, Line } from "react-native-svg";
+const FlavorBotLogoBNW = (props) => (
+  <Svg
+    width={25}
+    height={29}
+    viewBox="0 0 25 29"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <Rect x={0.5} y={4.5} width={22} height={14} rx={1.5} stroke="#888888" />
+    <Rect x={9} y={1} width={5} height={3} fill="#2A2A2A" stroke="#888888" />
+    <Mask id="path-3-inside-1_122_132" fill="white">
+      <Path d="M6 18H10C10.5523 18 11 18.4477 11 19V28C11 28.5523 10.5523 29 10 29H7C6.44772 29 6 28.5523 6 28V18Z" />
+    </Mask>
+    <Path
+      d="M5 17H9C10.1046 17 11 17.8954 11 19C11 19 10.5523 19 10 19H7L5 17ZM11 28C11 29.1046 10.1046 30 9 30H7C5.89543 30 5 29.1046 5 28H7H10C10.5523 28 11 28 11 28ZM7 30C5.89543 30 5 29.1046 5 28V17L7 19V28V30ZM11 18V29V18Z"
+      fill="#888888"
+      mask="url(#path-3-inside-1_122_132)"
+    />
+    <Rect x={2.5} y={21.5} width={2} height={5} rx={0.5} stroke="#888888" />
+    <Rect
+      x={19.5}
+      y={23.5}
+      width={2}
+      height={5}
+      rx={0.5}
+      transform="rotate(-90 19.5 23.5)"
+      stroke="#888888"
+    />
+    <Mask id="path-7-inside-2_122_132" fill="white">
+      <Path d="M13 19C13 18.4477 13.4477 18 14 18H18V28C18 28.5523 17.5523 29 17 29H14C13.4477 29 13 28.5523 13 28V19Z" />
+    </Mask>
+    <Path
+      d="M13 19C13 17.8954 13.8954 17 15 17H19L17 19H14C13.4477 19 13 19 13 19ZM19 28C19 29.1046 18.1046 30 17 30H15C13.8954 30 13 29.1046 13 28C13 28 13.4477 28 14 28H17H19ZM13 29V18V29ZM19 17V28C19 29.1046 18.1046 30 17 30V28V19L19 17Z"
+      fill="#888888"
+      mask="url(#path-7-inside-2_122_132)"
+    />
+    <Mask id="path-9-inside-3_122_132" fill="white">
+      <Path d="M10 18H14V25H10V18Z" />
+    </Mask>
+    <Path
+      d="M10 18V19H14V18V17H10V18ZM14 25V24H10V25V26H14V25Z"
+      fill="#888888"
+      mask="url(#path-9-inside-3_122_132)"
+    />
+    <Rect x={9.5} y={7.5} width={3} height={5} rx={0.5} stroke="#888888" />
+    <Rect x={16.5} y={7.5} width={3} height={5} rx={0.5} stroke="#888888" />
+    <Line x1={10.5} y1={25} x2={10.5} y2={28} stroke="#888888" />
+    <Line x1={13.5} y1={25} x2={13.5} y2={28} stroke="#888888" />
+  </Svg>
+);
+export default FlavorBotLogoBNW;
