b112064371920b66cf06141660fc40ae532120c8
ronald2117 <abel.ronald2117@gmail.com>
2025-06-06 21:11:34 +0800

    Refactor CustomSplashScreen: move styles definition inside the component for better encapsulation

--- FlavorFind/screens/CustomSplashScreen.js
+++ FlavorFind/screens/CustomSplashScreen.js
@@ -14,31 +14,33 @@ const CustomSplashScreen = ({ onReady }) => {
     prepare();
   }, []);
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: theme.background,
+    },
+    image: {
+      width: 220,
+      height: 200,
+      marginBottom: 20,
+    },
+    text: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#00',
+    },
+  });
+
   return (
     <View style={styles.container}>
-        <Image source={theme.mode == 'light' ? require('../assets/welcome-icon.png') : require('../assets/welcome-icon-dark-mode.png')}  width={220} height={220} />
+      <Image source={theme.mode == 'light' ? require('../assets/welcome-icon.png') : require('../assets/welcome-icon-dark-mode.png')}  width={220} height={220} />
       <Text style={styles.text}>Nomadic. Nostalgic. New.</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  image: {
-    width: 220,
-    height: 200,
-    marginBottom: 20,
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#00',
-  },
-});
+
 
 export default CustomSplashScreen;
